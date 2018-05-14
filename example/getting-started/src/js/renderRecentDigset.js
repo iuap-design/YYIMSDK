5bb753b2c3b73417d6de9120f70ace326cb90a02
zhouleim <zhouleim@yonyou.com>
2018-05-14 19:28:38 +0800

    更新样式

--- example/getting-started/src/js/renderRecentDigset.js
+++ example/getting-started/src/js/renderRecentDigset.js
@@ -18,7 +18,31 @@ const replaceEmoji = (str) => {
         return e;
     });
 };
-
+const  getNameLastTwo = (str) =>{
+    if (str) {
+        return str.slice(-2);
+    } else {
+        return "";
+    }    
+}
+const getColor = (name) =>{
+    let  color = ['#eead10', '#f99a2b', '#f38134', '#6495ed', '#3ab1aa', '#0abfb5', '#06aae1', '#00bfff', '#96bc53', '#00ced1', '#89a8e0'];
+    let newName = encodeURI(name).replace(/%/g, "");
+    let  lastName,
+        hexadecimal,
+        tenBinary;
+    if (newName.length >= 6) {
+        lastName = newName.substr(lastName, 6);
+        hexadecimal = parseInt(lastName, 16);
+        tenBinary = hexadecimal % 10;
+        if (isNaN(tenBinary)) {
+            return color[4];
+        }
+        return color[tenBinary]
+    } else {
+        return color[4]
+    }
+}
 export default (digsets) => {
     //拿取聊天对方id
     let targetuserid = localStorage.getItem('targetuserid');
@@ -37,16 +61,30 @@ export default (digsets) => {
         if(noreadno){
             newtipStr = '<i class="newtip cuttxt">'+ noreadno +'</i>';
         }
-        digStr += `<li class="${targetuserid && targetuserid === res.id ? 'active' : ''}" data-sessionVersion="${res.sessionVersion}" data-id="${res.id}" data-type="${res.type}" data-nickname="${res.nickname || res.id}">
-                    <i data-id="${res.id}" data-type="${res.type}" class="close">×</i>
-                    <div class="avatar">
-                        <img src="${YYIMChat.getFileUrl(res.photo) || './imgs/avatar.jpg'}" alt="">
-                    </div>
-                    <div class="detail">
-                        <h3 class="name cuttxt">${res.nickname || res.id}</h3>
-                        <p class="msg cuttxt">${replaceEmoji(lastmsgStr)}</p>
-                    </div>${newtipStr}
-                </li>`;
+        if(res.photo){
+            digStr += `<li class="${targetuserid && targetuserid === res.id ? 'active' : ''}" data-sessionVersion="${res.sessionVersion}" data-id="${res.id}" data-type="${res.type}" data-nickname="${res.nickname || res.id}">
+            <i data-id="${res.id}" data-type="${res.type}" class="close">×</i>
+            <div class="avatar">
+                <img src="${YYIMChat.getFileUrl(res.photo)}" alt="">
+            </div>
+            <div class="detail">
+                <h3 class="name cuttxt">${res.nickname || res.id}</h3>
+                <p class="msg cuttxt">${replaceEmoji(lastmsgStr)}</p>
+            </div>${newtipStr}
+          </li>`;
+        }else{
+            digStr += `<li class="${targetuserid && targetuserid === res.id ? 'active' : ''}" data-sessionVersion="${res.sessionVersion}" data-id="${res.id}" data-type="${res.type}" data-nickname="${res.nickname || res.id}">
+            <i data-id="${res.id}" data-type="${res.type}" class="close">×</i>
+            <div class="avatar avatarName"  style="background:${getColor(res.nickname)||getColor(res.id)}">
+                 ${getNameLastTwo(res.nickname) || getNameLastTwo(res.id)}
+            </div>
+            <div class="detail">
+                <h3 class="name cuttxt">${res.nickname || res.id}</h3>
+                <p class="msg cuttxt">${replaceEmoji(lastmsgStr)}</p>
+            </div>${newtipStr}
+          </li>`;
+        }
+       
     });
     $hcontacts.html(digStr);
 }
