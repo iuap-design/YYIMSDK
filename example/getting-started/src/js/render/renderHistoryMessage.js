e642c802363848e5e21e64197f9443cbc0bf46f3
zhouleim <zhouleim@yonyou.com>
2018-05-29 14:23:24 +0800

    解决群聊bug

--- example/getting-started/src/js/render/renderHistoryMessage.js
+++ example/getting-started/src/js/render/renderHistoryMessage.js
@@ -1,4 +1,3 @@
-
 //dom元素
 import {
     $chats,
@@ -9,16 +8,20 @@ import {
 import getRecentDigset from '../api/getRecentDigset';
 
 //渲染最近联系人函数
-import {renderRecentDigset} from './renderRecentDigset';
+import {
+    renderRecentDigset
+} from './renderRecentDigset';
 
 //表情数据
-import { expressionList } from '../common/constants';
+import {
+    expressionList
+} from '../common/constants';
 
 //用图片替换文本消息中表情信息
 const replaceEmoji = (str) => {
-    return str.replace(/\[[^\[\]]+\]/g,(e) => {
-        for (let i=0;i<expressionList.data.length;i++){
-            if(expressionList.data[i].actionData === e){
+    return str.replace(/\[[^\[\]]+\]/g, (e) => {
+        for (let i = 0; i < expressionList.data.length; i++) {
+            if (expressionList.data[i].actionData === e) {
                 return `<img class="emoji" src="${expressionList.path + expressionList.data[i].url}" alt="" />`;
                 break;
             }
@@ -44,19 +47,19 @@ export default (msg) => {
     let msgfromid = '';
 
     //如果msg存在，说明我正在发送消息或者我接收到了别人的消息
-    if(msg){
+    if (msg) {
         let isfromme;
-        if(chattype === 'chat'){
+        if (chattype === 'chat') {
             msgfromid = chattype === 'chat' ? msg.from : msg.from.roster;
-             isfromme = myid === msgfromid;
-        }else{
-            msgfromid =  msg.from.room;
+            isfromme = myid === msgfromid;
+        } else {
+            msgfromid = msg.from.room;
             let sendMsgId = msg.from.roster;
-             isfromme = myid === sendMsgId;
+            isfromme = myid === sendMsgId;
         }
-        if(isfromme){ //消息是我发给别人的
-            recentDigset.forEach(function(digest, i){
-                if(digest.id === targetuserid){
+        if (isfromme) { //消息是我发给别人的
+            recentDigset.forEach(function (digest, i) {
+                if (digest.id === targetuserid) {
                     recentDigset[i].lastContactTime = msg.data.dateline;
                     recentDigset[i].lastMessage = msg;
                     recentDigset[i].sessionVersion++;
@@ -67,55 +70,26 @@ export default (msg) => {
                     renderRecentDigset(recentDigset);
                 }
             });
-            //获取发送的人员头像和姓名
-            // YYIMChat.getVCard({
-            //     id: chatId,
-            //     success: function(res){
-            //         //整理最近联系人列表到一个新数组
-            //         historychats.push({
-            //             data: chat.data,
-            //             dateline:chat.dateline,
-            //             from:chat.from,
-            //             id:chat.id,
-            //             sessionVersion:chat.sessionVersion,
-            //             to:chat.to,
-            //             type:chat.type,
-            //             photo: res.photo || '',
-            //             nickname: res.nickname || res.id,
-            //         });
-            //               //把聊天记录缓存到本地
-            //             localStorage.setItem('historychats', JSON.stringify(historychatsData));
-            //             renderHistoryMessage();
-
-                
-            //     },
-            //     error:function(err){
-            //             //把聊天记录缓存到本地
-            //           localStorage.setItem('historychats', JSON.stringify(historychatsData));
-            //           renderHistoryMessage();
-            //         console.log(err);
-            //     }
-            // });
             //修改历史消息
             let userVcard = JSON.parse(localStorage.getItem('currentuserinfo') || "{}");
 
             historychats.push({
                 data: msg.data,
-                dateline:msg.dateline,
-                from:msg.from,
-                id:msg.id,
-                sessionVersion:msg.sessionVersion,
-                to:msg.to,
-                type:msg.type,
+                dateline: msg.dateline,
+                from: msg.from,
+                id: msg.id,
+                sessionVersion: msg.sessionVersion,
+                to: msg.to,
+                type: msg.type,
                 photo: userVcard.photo || '',
                 nickname: userVcard.nickname || userVcard.id,
             });
             //修改后保存
-            localStorage.setItem('historychats',JSON.stringify(historychats));
+            localStorage.setItem('historychats', JSON.stringify(historychats));
         } else { //消息来自于他人给我发的
             let isdigset = false; //判断对方在不在我的最近联系人里
-            recentDigset.forEach(function(digest, i){
-                if(digest.id === msgfromid){
+            recentDigset.forEach(function (digest, i) {
+                if (digest.id === msgfromid) {
                     isdigset = true;
                     recentDigset[i].lastContactTime = msg.data.dateline;
                     recentDigset[i].lastMessage = msg;
@@ -128,26 +102,122 @@ export default (msg) => {
                 }
             });
             //不在最近联系人中，刷新最近联系人列表
-            if(!isdigset){getRecentDigset();}
-            //修改历史消息
-            historychats.push(msg);
-            //修改后保存
-            localStorage.setItem('historychats',JSON.stringify(historychats));
-            
+            if (!isdigset) {
+                getRecentDigset();
+            };
+            let sendFromId;
+            if (chattype === 'chat') {
+                sendFromId = msg.from;
+            } else {
+                sendFromId = msg.from.roster;
+            }
+            //获取发送的人员头像和姓名
+            YYIMChat.getVCard({
+                id: sendFromId,
+                success: function (res) {
+                    //整理最近联系人列表到一个新数组
+                    historychats.push({
+                        data: msg.data,
+                        dateline: msg.dateline,
+                        from: msg.from,
+                        id: msg.id,
+                        sessionVersion: msg.sessionVersion,
+                        to: msg.to,
+                        type: msg.type,
+                        photo: res.photo || '',
+                        nickname: res.nickname || res.id,
+                    });
+                    //修改历史消息
+                    //historychats.push(msg);
+                    //修改后保存
+                    localStorage.setItem('historychats', JSON.stringify(historychats));
+
+
+                    let chatsStr = '';
+                    historychats.forEach(function (chat, i) {
+                        let isfromme = chattype === 'chat' ? myid === chat.from : myid === chat.from.roster;
+                        // let chatfrom = chattype === 'chat' ? '' : `<div class="chat-user-name">${chat.from.roster}</div>`;
+                        let chatfrom = chat.nickname;
+                        //文本消息
+                        if (chat.data.contentType === 2) {
+                            chatsStr += `<li>
+                            <div class="chat-tip">${new Date(chat.data.dateline).toLocaleTimeString()}</div>
+                            <div class="chat-content">
+                                <div class="${ isfromme? 'chat-avatar chat-avatar-send' :'chat-avatar'}">
+                                    <img src=${YYIMChat.getFileUrl(chat.photo)||'./imgs/avatar.jpg'} alt="">
+                                </div>
+                                <div class="${ isfromme? 'chat-txt chat-txt-send' :'chat-txt'}">
+                                    ${chatfrom}
+                                    <div class="chat-msg">${replaceEmoji(chat.data.content)}</div>
+                                </div>
+                            </div>
+                        </li> `;
+                        } else if (chat.data.contentType === 8) { //图片消息
+                            let picurl = YYIMChat.getFileUrl(chat.data.content.attachId);
+                            chatsStr += `<li>
+                            <div class="chat-tip">${new Date(chat.data.dateline).toLocaleTimeString()}</div>
+                            <div class="chat-content">
+                                <div class="${ isfromme? 'chat-avatar chat-avatar-send' :'chat-avatar'}">
+                                    <img src=${YYIMChat.getFileUrl(chat.photo)||'./imgs/avatar.jpg'} alt="">
+                                </div>
+                                <div class="${ isfromme? 'chat-txt chat-txt-send' :'chat-txt'}">
+                                    ${chatfrom}
+                                    <div class="chat-msg">
+                                        <img class="chatpic" data-url="${picurl}" src="${picurl}" title="点击查看图片" alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </li> `;
+                        } else if (chat.data.contentType === 4) {
+                            let picurl = YYIMChat.getFileUrl(chat.data.content.attachId);
+                            let filename = chat.data.content.name.slice(0, 14);
+                            chatsStr += `<li>
+                            <div class="chat-tip">${new Date(chat.data.dateline).toLocaleTimeString()}</div>
+                            <div class="chat-content">
+                                <div class="${ isfromme? 'chat-avatar chat-avatar-send' :'chat-avatar'}">
+                                    <img src=${YYIMChat.getFileUrl(chat.photo)||'./imgs/avatar.jpg'} alt="">
+                                </div>
+                                <div class="${ isfromme? 'chat-txt chat-txt-send' :'chat-txt'}">
+                                    ${chatfrom}
+                                    <div class="chat-msg">
+                                        <a class="chatfile" href="${picurl}" title="点击下载文件">
+                                            <span class="filename">${filename}</span>
+                                            <span class="filesize">${chat.data.content.size}B</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li> `;
+                        }
+                    });
+                    $chats_list.html(chatsStr);
+                    $chats.scrollTop($chats[0].scrollHeight);
+                    return ;
+
+                },
+                error: function (err) {
+                    //把聊天记录缓存到本地
+                    localStorage.setItem('historychats', JSON.stringify(historychatsData));
+                    renderHistoryMessage();
+                    console.log(err);
+                }
+            });
+
+            return;
         }
-       
-        
+
+
     }
     //如果我没和对方聊天，则不渲染历史信息
-    if(msg && msgfromid !== myid && msgfromid !== targetuserid) return;
+    if (msg && msgfromid !== myid && msgfromid !== targetuserid) return;
 
     let chatsStr = '';
-    historychats.forEach(function(chat, i){
+    historychats.forEach(function (chat, i) {
         let isfromme = chattype === 'chat' ? myid === chat.from : myid === chat.from.roster;
-       // let chatfrom = chattype === 'chat' ? '' : `<div class="chat-user-name">${chat.from.roster}</div>`;
-       let chatfrom = chat.nickname;
+        // let chatfrom = chattype === 'chat' ? '' : `<div class="chat-user-name">${chat.from.roster}</div>`;
+        let chatfrom = chat.nickname;
         //文本消息
-        if(chat.data.contentType === 2){
+        if (chat.data.contentType === 2) {
             chatsStr += `<li>
                             <div class="chat-tip">${new Date(chat.data.dateline).toLocaleTimeString()}</div>
                             <div class="chat-content">
@@ -160,7 +230,7 @@ export default (msg) => {
                                 </div>
                             </div>
                         </li> `;
-        }else if(chat.data.contentType === 8){  //图片消息
+        } else if (chat.data.contentType === 8) { //图片消息
             let picurl = YYIMChat.getFileUrl(chat.data.content.attachId);
             chatsStr += `<li>
                             <div class="chat-tip">${new Date(chat.data.dateline).toLocaleTimeString()}</div>
@@ -176,7 +246,7 @@ export default (msg) => {
                                 </div>
                             </div>
                         </li> `;
-        }else if(chat.data.contentType === 4){
+        } else if (chat.data.contentType === 4) {
             let picurl = YYIMChat.getFileUrl(chat.data.content.attachId);
             let filename = chat.data.content.name.slice(0, 14);
             chatsStr += `<li>
